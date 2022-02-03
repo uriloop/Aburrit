863d865a4e01529b52a8e4e18ab3d93b2718fe0c
uriloop <orlopez@elpuig.xeill.net>
2022-02-03 18:00:48 +0100

    Afegint taula participants

--- src/main/java/elpuig/uri/aburrit/GraficController.java
+++ src/main/java/elpuig/uri/aburrit/GraficController.java
@@ -21,9 +21,11 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.ResourceBundle;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class GraficController implements Initializable {
@@ -112,26 +114,39 @@ public class GraficController implements Initializable {
         dificultat.getData().add(barraMig);
         dificultat.getData().add(barraDificil);
 
-        // iniciem el grafic de participants
+        /*// iniciem el grafic de participants
+        Collection<Bored> integers=boreds.getBoreds().stream().collect(Collectors.toMap(Bored::getParticipants, p -> p, (p, q) -> p)).values();
+        int numOpcions=integers.size();
+        List<XYChart.Series<String,Integer>> participantsXYCharts= new ArrayList<>();
+        for (Bored bor:
+             integers) {
+            participantsXYCharts.add(new XYChart.Series<Integer,Integer>().getData().add(new XYChart.Data<Integer,Integer>(
+                    bor.getParticipants(),(int)integers.stream().filter(a -> a.getParticipants()==bor.getParticipants())
+            )));
+        }
+        */
+        /*List<Bored> bors=boreds.getBoreds().stream().filter(distinctByKey(p -> p.getFname()).distinct().collect(Collectors.toList());
 
         Stream<Bored> distincts=boreds.getBoreds()
                 .stream()
                 .distinct();
-        List<XYChart.Series<String,Integer>> participantsXYCharts= new ArrayList<>();
+        List<XYChart.Series<String,Integer>> participantsXYCharts= new ArrayList<>();*/
         // per cada numero de participants diferent.
-        for (Bored bor:
+        /*for (Bored bor:
              distincts.toList()) {
             // per cada opció diferent he de fer un label de tipus de valor, més tard per cada contar tots els casos en que el valor és igual a aquest label.
             // és a dir. fer un distinct per quedar-me totes les opcions possibles. després fer un count on filter = a.getParticipants==label.getParticipants();
-            /*participantsXYCharts.add(
+            *//* participantsXYCharts.add(
                     new XYChart.Series<Integer,Integer>().getData().add( new XYChart.Data<Integer,Integer>((int)bor.getParticipants(),
                             (int) boreds.getBoreds()
                             .stream()
-                            .filter(a -> a.getParticipants()== bor.getParticipants()).count()).));*/
-        }
+                            .filter(a -> a.getParticipants()== bor.getParticipants()).count())));*//*
+
+        }*/
 
 
     }
+
 
     @FXML
     public void closeApp(Event event) {
